08f4a371aeb9283c0133473f8c3526b353580832
Damien Whitten <dw@chaos404.com>
2024-01-25 10:24:59 -0800

    Import j5 types from moretypes

---
+++ j5types/decimal.go
@@ -0,0 +1,11 @@
+package j5types
+
+import "math/big"
+
+func (d *Decimal) AsBig() *big.Rat {
+	val, ok := new(big.Rat).SetString(d.String_)
+	if !ok {
+		panic("Invalid Decimal value")
+	}
+	return val
+}
