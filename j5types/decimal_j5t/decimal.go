9f3be5d5833b83ad00fff83048c99557ec95012b
Damien Whitten <dw@chaos404.com>
2024-09-08 23:10:56 -0700

    Gen Fixes

--- j5types/decimal_j5t/decimal.go
+++ j5types/decimal_j5t/decimal.go
@@ -10,6 +10,29 @@ func (d *Decimal) ToShop() (decimal.Decimal, error) {
 	return decimal.NewFromString(d.Value)
 }
 
+func (d *Decimal) Decimal() decimal.Decimal {
+	sd, err := d.ToShop()
+	if err != nil {
+		panic(err)
+	}
+	return sd
+}
+
+func (d *Decimal) Neg() *Decimal {
+	sd := d.Decimal().Neg()
+	return FromShop(sd)
+}
+
+func (d *Decimal) Add(d2 *Decimal) *Decimal {
+	sd := d.Decimal().Add(d2.Decimal())
+	return FromShop(sd)
+}
+
+func (d *Decimal) Sub(d2 *Decimal) *Decimal {
+	sd := d.Decimal().Sub(d2.Decimal())
+	return FromShop(sd)
+}
+
 func FromShop(d decimal.Decimal) *Decimal {
 	return &Decimal{
 		Value: d.String(),
