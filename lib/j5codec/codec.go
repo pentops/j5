7a5fb51034135b57e543cac9a2ba87ac1e6854c0
Damien Whitten <dw@chaos404.com>
2025-01-19 09:24:12 -0800

    External options

--- lib/j5codec/codec.go
+++ lib/j5codec/codec.go
@@ -12,3 +12,16 @@ func NewCodec(opts ...Option) *Codec {
 	cc := codec.NewCodec(opts...)
 	return cc
 }
+
+type MessageTypeResolver = codec.MessageTypeResolver
+type CodecOption = codec.CodecOption
+
+// WithResolver sets a custom resolver for decoding any types.
+func WithResolver(resolver MessageTypeResolver) CodecOption {
+	return codec.WithResolver(resolver)
+}
+
+// WithProtoToAny adds a proto encoding to j5.any.v1 messages using the resolver
+func WithProtoToAny() CodecOption {
+	return codec.WithProtoToAny()
+}
