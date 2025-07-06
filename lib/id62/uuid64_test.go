d90f52d3e066f6c233a7fa8e9449728d324b9fe4
dan9186 <dan9186@gmail.com>
2025-07-06 12:49:51 -0700

    explain what's going on better

--- lib/id62/uuid64_test.go
+++ lib/id62/uuid64_test.go
@@ -6,43 +6,69 @@ import (
 )
 
 func TestEdges(t *testing.T) {
+	tcs := []struct {
+		name       string
+		uuid       UUID
+		expId62Str string
+		expB64Str  string
+	}{
+		{
+			name:       "Zero",
+			uuid:       UUID{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+			expId62Str: "0000000000000000000000",
+			expB64Str:  "AAAAAAAAAAAAAAAAAAAAAA==",
+		},
+		{
+			name:       "Leading One",
+			uuid:       UUID{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+			expId62Str: "01SZwviYzes2mjOamuMJWw",
+			expB64Str:  "AQAAAAAAAAAAAAAAAAAAAA==",
+		},
+		{
+			name:       "One",
+			uuid:       UUID{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+			expId62Str: "0000000000000000000001",
+			expB64Str:  "AAAAAAAAAAAAAAAAAAAAAQ==",
+		},
+		{
+			name:       "Max",
+			uuid:       UUID{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff},
+			expId62Str: "7N42dgm5tFLK9N8MT7fHC7",
+			expB64Str:  "/////////////////////w==",
+		},
+	}
 
-	for _, tc := range []struct {
-		uuid UUID
-		str  string
-	}{{
-		uuid: UUID{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-		str:  "0000000000000000000000",
-	}, {
-		uuid: UUID{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	}, {
-		uuid: UUID{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		str:  "0000000000000000000001",
-	}, {
-		uuid: UUID{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff},
-	}} {
-		t.Run(fmt.Sprintf("%x", tc.uuid), func(t *testing.T) {
-			asStr := tc.uuid.String()
-			t.Logf("asStr: %q", asStr)
-			if tc.str != "" {
-				if asStr != tc.str {
-					t.Errorf("expected %q, got %q", tc.str, asStr)
-				}
-			}
-			if len(asStr) != 22 {
-				t.Errorf("expected 22, got %d", len(asStr))
-			}
-			if !Pattern.MatchString(asStr) {
+	for _, tc := range tcs {
+		t.Run(fmt.Sprintf("%s", tc.name), func(t *testing.T) {
+			str := tc.uuid.String()
+			t.Logf("uuid: %s", tc.uuid.UUIDString())
+			t.Logf("id62: %s", str)
+			t.Logf("base64: %s", tc.uuid.Base64String())
+
+			// Regex match
+			if !Pattern.MatchString(str) {
 				t.Errorf("expected match, got no match")
 			}
-			asID, err := Parse(asStr)
+
+			// Deterministic output
+			if str != tc.expId62Str {
+				t.Errorf("expected %q, got %q", tc.expId62Str, str)
+			}
+
+			if tc.uuid.Base64String() != tc.expB64Str {
+				t.Errorf("expB64 %q, got %q", tc.expB64Str, tc.uuid.Base64String())
+			}
+
+			// Can parse from ID62 string
+			asID, err := Parse(str)
 			if err != nil {
 				t.Errorf("unexpected error: %v", err)
 			}
+
+			// Parsed matches original
 			if asID != tc.uuid {
 				t.Errorf("expected %v, got %v", tc.uuid, asID)
 			}
 		})
 	}
-
 }
