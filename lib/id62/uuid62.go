3271d4debfbb126520da6357f1d13c72c0fe232a
dan9186 <dan9186@gmail.com>
2025-07-06 12:55:23 -0700

    Expand id62 tests & add uuid parsing (#191)
    * spacing for readability
    
    * explain what's going on better
    
    * add parsing from a uuid
    
    * it's already a string

--- lib/id62/uuid62.go
+++ lib/id62/uuid62.go
@@ -54,6 +54,18 @@ func Parse(s string) (UUID, error) {
 	if err := parseBase62(s, uuid[:]); err != nil {
 		return UUID{}, err
 	}
+
+	return uuid, nil
+}
+
+func ParseUUID(s string) (UUID, error) {
+	u, err := uuid.Parse(s)
+	if err != nil {
+		return UUID{}, fmt.Errorf("cannot parse UUID: %w", err)
+	}
+
+	var uuid UUID
+	copy(uuid[:], u[:])
 	return uuid, nil
 }
 
@@ -61,20 +73,24 @@ func base62String(id []byte) string {
 	var i big.Int
 	i.SetBytes(id)
 	str := i.Text(62)
+
 	if len(str) < 22 {
 		str = fmt.Sprintf("%022s", str)
 	} else if len(str) > 22 {
 		panic("base62 value is too large")
 	}
+
 	return str
 }
 
 func parseBase62(s string, into []byte) error {
 	var i big.Int
+
 	_, ok := i.SetString(s, 62)
 	if !ok {
 		return fmt.Errorf("cannot parse base62: %q", s)
 	}
+
 	valBytes := i.Bytes()
 	if len(valBytes) > len(into) {
 		return fmt.Errorf("base62 value is too large: %d > %d", len(valBytes), len(into))
@@ -84,5 +100,6 @@ func parseBase62(s string, into []byte) error {
 	} else {
 		copy(into, valBytes)
 	}
+
 	return nil
 }
